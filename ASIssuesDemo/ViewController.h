8a3e1cc04c7499ed0f8b9ff400cdd0af8f100f8e
Jerome Miglino <jerome.miglino@gmail.com>
2016-04-12 10:20:58 -0400

    Initial Commit + 1.9.6 ASyncDisplayKit

---
+++ ASIssuesDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASIssuesDemo
+//
+//  Created by jmig on 4/12/16.
+//  Copyright © 2016 Jmig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+
+pod 'AsyncDisplayKit', '~> 1.9.6'
